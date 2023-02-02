34ff0c64129640e4316f55b8eb108bbfb565e815
Tyler <tyler.clark@touchintel.io>
2023-02-02 16:24:19 +0100

    example.py
    Updated a more complex example

--- src/sim/example.py
+++ src/sim/example.py
@@ -1,4 +1,5 @@
 import unittest;
+import numpy as np
 from environment import Environment;
 from namespace import Namespace;
 
@@ -7,48 +8,80 @@ class Example(unittest.TestCase):
     def test_environment_init(self):
         e = Environment()
 
-        def sim_blood_sugar_level(state, connected_state):
+        def wanting_carbs(state, connected_state):
+            l = connected_state[0].get('level')
+            w = 1/l
+            state.set('wanting', w)
+
+        cWanting = e.add_function(wanting_carbs, "seeking.carbs", ["physiology.blood.glucose"])
+        cWanting.set('wanting', 0)
+
+        def decision(state, connected_state):
+            w = connected_state[0].get('wanting')
+            v = connected_state[1].get('carb_volume')
+
+            if w > 10:
+                state.set('carbs', True)
+
+            if v > 4:
+                state.set('carbs', False)
+
+        cDecision = e.add_function(decision, "decision.eat", ["seeking.carbs",'stomach.contents'])
+        cDecision.set('carbs', False)
+
+        def carb_consumption(state, connected_state):
+            d = connected_state[0].get('carbs')
+            c = state.get('carb_volume')
+            c = c*0.95
+
+            if d:
+                c += 5
+            state.set('carb_volume', c)
+
+        cConsumption = e.add_function(carb_consumption, "stomach.contents", ["decision.eat", 'stomach.contents'])
+        cConsumption.set('carb_volume', 10)
+
+        def carb_metabolism(state, connected_state):
+            d = connected_state[0].get('carb_volume')
+            d2 = d*0.90
+            b_s_c = d-d2
             l = state.get('level')
-            l = l * 0.95
-
-            ate = state.get('just_ate')
-            if ate:
-                l = l + 100
-
+            l = l+b_s_c*2-l*(1-0.95)
             state.set('level', l)
-            state.set('just_ate', False)
 
-        sGlucose = e.add_function(sim_blood_sugar_level, "physiology.blood.glucose", [])
-        sGlucose.set('level', 1.0)
-        sGlucose.set('just_ate', False)
+        cMetabolism = e.add_function(carb_metabolism, 'physiology.blood.glucose', ["stomach.contents"])
+        cMetabolism.set('level',1)
 
-        def sim_salience_smell_of_pizza(state, connected_state):
-            stateG = connected_state.pop()
-            l = stateG.get('level')
-            s = state.get('need')
-            s = s * 1.0 / l
-            state.set('need', s)
+        '''
+        def reduce_carb(state, connected_state):
+            d = state.get('carb_volume')
+            d = d*0.75
+            state.set('carb_volume', d)
 
-        sSalience = e.add_function(sim_salience_smell_of_pizza, "physiology.brain.salience", ['physiology.blood.glucose'])
-        sSalience.set('need', 10.0)
+        cReduce = e.add_function(reduce_carb, 'stomach.contents', [])
+        cReduce.set('carb_volume',10)
+        '''
 
         def probe_glucose(level):
             print("Blood Glucose Level {}".format(level))
 
-        def probe_need(need):
-            print("Need {}".format(need))
+        def probe_wanting(want):
+            print("Wanting carbs {}".format(want))
+
+        def probe_carb_volume(vol):
+            print("Stomach carb volume {}".format(vol))
+
+        def probe_eat_decision(d):
+            print("Stomach eat decision {}".format(d))
 
         e.add_probe('physiology.blood.glucose', 'level', probe_glucose)
-        e.add_probe('physiology.brain.salience', 'need', probe_need)
+        e.add_probe('seeking.carbs', 'wanting', probe_wanting)
+        e.add_probe('stomach.contents', 'carb_volume', probe_carb_volume)
+        e.add_probe('decision.eat', 'carbs', probe_eat_decision)
 
-        for i in range(0, 10):
+        for i in range(0, 1000):
             e.tick()
 
-        i_eat_pizza = e.get_injector('physiology.blood.glucose', 'just_ate')
-        i_eat_pizza.set(True)
-
-        for i in range(0, 10):
-            e.tick()
 
 if __name__ == '__main__':
     unittest.main()
